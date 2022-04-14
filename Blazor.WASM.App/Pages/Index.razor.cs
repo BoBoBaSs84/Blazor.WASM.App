4ecbc759f020bd64d2b3249686310b2a1e3d2e81
Bobo <Bobo@BOBO-DEV>
2022-04-14 15:38:19 +0200

    well, let's do this

---
+++ Blazor.WASM.App/Pages/Index.razor.cs
@@ -0,0 +1,39 @@
+﻿using Manufaktura.Controls.Extensions;
+using Manufaktura.Controls.Model;
+using Manufaktura.Controls.Model.Fonts;
+using Manufaktura.Controls.Rendering;
+using Manufaktura.Controls.Rendering.Implementations;
+using Manufaktura.Music.Model;
+using Manufaktura.Music.Model.MajorAndMinor;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.WASM.App.Pages
+{
+	public partial class Index : ComponentBase
+	{
+		private readonly Score score = Score.CreateOneStaffScore(Clef.Treble, MajorScale.C);
+		private readonly HtmlScoreRendererSettings settings = new() { RenderSurface = HtmlScoreRendererSettings.HtmlRenderSurface.Svg };
+
+		private void AddNote() => score.FirstStaff.Elements.Add(new Note(Pitch.G4, RhythmicDuration.Quarter));
+
+		protected override void OnInitialized()
+		{
+			score.FirstStaff.AddRange(StaffBuilder
+					.FromPitches(Pitch.C4, Pitch.D4, Pitch.E4, Pitch.F4, Pitch.G4, Pitch.E4)
+					.AddRhythm("8 8 8 8 4 4"));
+			string[]? musicFontUris = new[] { "/fonts/Polihymnia.svg", "/fonts/Polihymnia.ttf", "/fonts/Polihymnia.woff" };
+			settings.RenderingMode = ScoreRenderingModes.AllPages;
+			settings.Fonts.Add(MusicFontStyles.MusicFont, new HtmlFontInfo("Polihymnia", 22, musicFontUris));
+			settings.Fonts.Add(MusicFontStyles.StaffFont, new HtmlFontInfo("Polihymnia", 24, musicFontUris));
+			settings.Fonts.Add(MusicFontStyles.GraceNoteFont, new HtmlFontInfo("Polihymnia", 14, musicFontUris));
+			settings.Fonts.Add(MusicFontStyles.LyricsFont, new HtmlFontInfo("Open Sans", 9, "/fonts/OpenSans-Regular.ttf"));
+			settings.Fonts.Add(MusicFontStyles.TimeSignatureFont, new HtmlFontInfo("Open Sans", 12, "/fonts/OpenSans-Regular.ttf"));
+			settings.Fonts.Add(MusicFontStyles.DirectionFont, new HtmlFontInfo("Open Sans", 10, "/fonts/OpenSans-Regular.ttf"));
+			settings.Scale = 1;
+			settings.CustomElementPositionRatio = 0.8;
+			settings.IgnorePageMargins = true;
+
+			base.OnInitialized();
+		}
+	}
+}
---
+++ Blazor.WASM.App/Shared/NoteViewer.razor.cs
@@ -0,0 +1,37 @@
+﻿using Manufaktura.Controls.Model;
+using Manufaktura.Controls.Rendering.Implementations;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.WASM.App.Shared
+{
+	public partial class NoteViewer
+	{
+		[Parameter] public Score? Score { get; set; }
+		[Parameter] public HtmlScoreRendererSettings? Settings { get; set; }
+
+		private int canvasIdCount = 0;
+
+		public string RenderScore()
+		{
+			IScore2HtmlBuilder builder;
+
+			if (Score is null)
+				throw new ArgumentNullException(nameof(Score));
+
+			if (Settings is null)
+				throw new ArgumentNullException(nameof(Settings));
+
+			if (Settings.RenderSurface == HtmlScoreRendererSettings.HtmlRenderSurface.Canvas)
+				builder = new Score2HtmlCanvasBuilder(Score, string.Format("scoreCanvas{0}", canvasIdCount), Settings);
+			else if (Settings.RenderSurface == HtmlScoreRendererSettings.HtmlRenderSurface.Svg)
+				builder = new Score2HtmlSvgBuilder(Score, string.Format("scoreCanvas{0}", canvasIdCount), Settings);
+			else throw new NotImplementedException("Unsupported rendering engine.");
+
+			string html = builder.Build();
+
+			canvasIdCount++;
+
+			return html;
+		}
+	}
+}
